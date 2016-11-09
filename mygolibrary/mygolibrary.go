82033b4f8fac02b23d25d0e0564b1981363c7cea
Daniel Esteban <conejo@conejo.me>
2016-11-09 19:09:16 +0100

    updated

--- mygolibrary/mygolibrary.go
+++ mygolibrary/mygolibrary.go
@@ -13,6 +13,12 @@ func Hello(name string) string {
 	return "Hello " + name + "! (from go)"
 }
 
+func HelloWithError(name string) (string, error) {
+    result := "Hello " + name + "! (from go)"
+    err := fmt.Errorf("ERROR %s is not a valid name", name)
+    return result, err
+}
+
 func HelloCallback(name string) {
     jc.CallFromGo("Bonjour " + name + "! (from go)")
 }
